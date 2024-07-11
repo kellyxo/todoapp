18b05cf4ac09376a46bc9915edbb001a4033f484
kellyxo <dude7037@gmail.com>
2024-07-10 20:49:04 -0500

    First commit

---
+++ src/main/java/com/example/todoapp/repository/ToDoRepository.java
@@ -0,0 +1,8 @@
+package com.example.todoapp.repository;
+
+import com.example.todoapp.entity.ToDo;
+
+public interface ToDoRepository extends JpaRepository<ToDo, Long>
+{
+
+}
---
+++ src/main/java/com/example/todoapp/service/ToDoService.java
@@ -0,0 +1,5 @@
+package com.example.todoapp.service;
+
+public class ToDoService
+{
+}
